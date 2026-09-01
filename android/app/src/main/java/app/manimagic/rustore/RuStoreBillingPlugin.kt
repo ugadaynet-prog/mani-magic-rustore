6280ed4c610b49d8936402c9dffd45bd1910d715
ugadaynet-prog <ugadaynet@gmail.com>
2026-09-01 09:57:25 +0300

    Fix: add missing imports for PluginCall, PluginMethod

--- android/app/src/main/java/app/manimagic/rustore/RuStoreBillingPlugin.kt
+++ android/app/src/main/java/app/manimagic/rustore/RuStoreBillingPlugin.kt
@@ -1,6 +1,8 @@
 package app.manimagic.rustore
 
 import com.getcapacitor.Plugin
+import com.getcapacitor.PluginCall
+import com.getcapacitor.PluginMethod
 import com.getcapacitor.annotation.CapacitorPlugin
 
 /**
